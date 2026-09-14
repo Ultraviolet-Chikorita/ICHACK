8fc0a16379f807424fa3f625c735bce884468747
UVCHIKORITA <147429542+Ultraviolet-Chikorita@users.noreply.github.com>
2026-09-14 16:14:33 +0100

    Protect submission and teacher API endpoints

--- ICHACK_0/ichacksite/ichacksite2/urls.py
+++ ICHACK_0/ichacksite/ichacksite2/urls.py
@@ -1,15 +1,37 @@
 from django.urls import path
+
 from . import views
+from .access import authenticated_view, teacher_required
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login', views.login_route, name='login_route'),
-    path('about', views.about, name='about'),
-    path('team', views.team, name='team'),
-    path('loginUser', views.loginUser, name='loginUser'),
-    path('getDetailsForCourse', views.getDetailsForCourse, name='getDetailsForCourse'),
-    path('getEssayQuestions', views.getEssayQuestions, name='getEssayQuestions'),
-    path('getQuestionsForCourse_teacher', views.getQuestionsForCourse_teacher, name='getQuestionsForCourse_teacher'),
-    path('getSubmissionsForQuestion_teacher', views.getSubmissionsForQuestion_teacher, name='getSubmissionsForQuestion_teacher'),
-    path('add_submission', views.add_submission, name="add_submission")
+    path("", views.index, name="index"),
+    path("login", views.login_route, name="login_route"),
+    path("about", views.about, name="about"),
+    path("team", views.team, name="team"),
+    path("loginUser", views.loginUser, name="loginUser"),
+    path(
+        "getDetailsForCourse",
+        authenticated_view(views.getDetailsForCourse),
+        name="getDetailsForCourse",
+    ),
+    path(
+        "getEssayQuestions",
+        authenticated_view(views.getEssayQuestions),
+        name="getEssayQuestions",
+    ),
+    path(
+        "getQuestionsForCourse_teacher",
+        teacher_required(views.getQuestionsForCourse_teacher),
+        name="getQuestionsForCourse_teacher",
+    ),
+    path(
+        "getSubmissionsForQuestion_teacher",
+        teacher_required(views.getSubmissionsForQuestion_teacher),
+        name="getSubmissionsForQuestion_teacher",
+    ),
+    path(
+        "add_submission",
+        authenticated_view(views.add_submission),
+        name="add_submission",
+    ),
 ]
